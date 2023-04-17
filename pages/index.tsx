de503a393194ae920346fc224ca7fcd8660f7597
Ioannis Chrysostomakis <ic768@protonmail.com>
2023-04-17 22:27:45 +0300

    Underline on hover in login

--- pages/index.tsx
+++ pages/index.tsx
@@ -64,12 +64,20 @@ export default function Home() {
             </div>
             <span>
               Don&apos;t have an account? Register
-              <Link
-                href="/register"
-                className="p-1 rounded-full text-emerald-300 transition-colors hover:text-emerald-500"
-              >
-                here
-              </Link>
+              <div className="relative inline">
+                <Link
+                  href="/register"
+                  className="p-1 rounded-full text-emerald-300 transition-colors hover:text-emerald-400
+
+              relative before:content-[''] before:absolute before:block before:w-full before:h-[1px]
+              before:bottom-0 before:left-1 before:bg-emerald-400
+              before:hover:scale-x-90 before:scale-x-0 before:origin-top-left
+              before:transition before:ease-in-out before:duration-300
+                  "
+                >
+                  here
+                </Link>
+              </div>
             </span>
             <button
               onClick={onLogin}
