33456e361a4f7a0411c5494b7761ef8589e8e254
Ioannis Chrysostomakis <ic768@protonmail.com>
2023-07-27 22:58:02 +0300

    Make notifications thinner

--- components/common/Notification/Notification.tsx
+++ components/common/Notification/Notification.tsx
@@ -18,7 +18,7 @@ const Notification = ({
 
   return (
     <div
-      className={`top-6 z-20 absolute gap-4 inset-0 flex items-center px-8 py-2 justify-center px-8 py-2 mx-auto bg-white outline ${fgColor} ${bgColor} ${outlineColor} rounded-md w-4/6 sm:w-2/6 h-28 sm:h-32 animation-fill-forwards animate-fade-notification-in`}
+      className={`top-4 z-20 absolute gap-4 inset-0 flex items-center px-8 py-2 justify-center px-8 py-2 mx-auto bg-white outline ${fgColor} ${bgColor} ${outlineColor} rounded-md w-4/6 sm:w-2/6 h-12 sm:h-16 animation-fill-forwards animate-fade-notification-in`}
     >
       <FontAwesomeIcon icon={icon} className="h-5/6" />
       <span>{message}</span>
