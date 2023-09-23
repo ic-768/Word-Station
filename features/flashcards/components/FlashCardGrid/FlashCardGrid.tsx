a84062311c944c146ebc8aa7c48e2e1144d510b8
Ioannis Chrysostomakis <ic768@protonmail.com>
2023-09-23 13:03:09 +0300

    useEffect for flashcard matching

--- features/flashcards/components/FlashCardGrid/FlashCardGrid.tsx
+++ features/flashcards/components/FlashCardGrid/FlashCardGrid.tsx
@@ -1,4 +1,4 @@
-import { Fragment, useMemo, useState } from "react";
+import { Fragment, useEffect, useMemo, useState } from "react";
 
 import { WordMeanings } from "features/words";
 import FlashCardGroup from "../FlashCardGroup";
@@ -17,8 +17,6 @@ const FlashCardGrid = ({
   // Current selections
   const [selectedWord, setSelectedWord] = useState<string>();
   const [selectedDefinition, setSelectedDefinition] = useState<string>();
-  // Words that user has correctly matched
-  const [matchedWord, setMatchedWord] = useState<string[]>([]);
 
   // array of objects with a definition, and the word that it belongs to
   const randomPairs = useMemo(
@@ -38,18 +36,17 @@ const FlashCardGrid = ({
   const getDefinitionStyle = (d: string) =>
     d && selectedDefinition === d ? "bg-orange-500" : "bg-slate-500 ";
 
-  // Check to see if currently selected word/definition are correct
-  // Because the user can start and end the match either from the word OR from the definition,
-  // we take either one as params to override the (stale) value in state.
-  const onMatch = ({ word, definition }: Partial<WordDefinitionPair>) => {
-    // TODO don't run if no selectedWord and no selectedDefinition (bangs should go entirely)
-    // or if double-clicking same thing
-    const predicate = word
-      ? { word, definition: selectedDefinition! }
-      : { word: selectedWord!, definition: definition! };
+  // Check to see if a definition and a word have been selected to check for a match
+  useEffect(() => {
+    if (!selectedWord || !selectedDefinition) return;
 
-    return isMatch(randomPairs, predicate);
-  };
+    console.log(
+      isMatch(randomPairs, {
+        word: selectedWord,
+        definition: selectedDefinition,
+      })
+    );
+  }, [selectedWord, selectedDefinition, randomPairs]);
 
   // we map over the randomized pair, and render the random word, with a definition that (probably) doesn't belong to it
   return (
@@ -65,8 +62,6 @@ const FlashCardGrid = ({
                 <button
                   onClick={() => {
                     setSelectedWord(word);
-                    // if we didn't give a param here and used selectedWord instead, selectedWord would be stale
-                    console.log(onMatch({ word }));
                   }}
                   className="flex-1"
                 >
@@ -79,7 +74,6 @@ const FlashCardGrid = ({
                 <button
                   onClick={() => {
                     setSelectedDefinition(definition);
-                    console.log(onMatch({ definition }));
                   }}
                   className="flex-1"
                 >
