bdea65419a2bd51326b1b04f13f07bcca3dddc88
Ioannis Chrysostomakis <ic768@protonmail.com>
2023-10-07 22:17:08 +0300

    Removes correct pair

--- features/flashcards/components/FlashCardGrid.tsx
+++ features/flashcards/components/FlashCardGrid.tsx
@@ -1,4 +1,4 @@
-import { Fragment, useEffect, useMemo, useState } from "react";
+import { Fragment, useCallback, useEffect, useState } from "react";
 
 import { WordMeanings } from "features/words";
 import FlashCardGroup from "./FlashCardGroup";
@@ -14,18 +14,33 @@ const FlashCardGrid = ({
   group: { title, words },
   meanings,
 }: FlashCardGridProps) => {
+  // Use these to display words and meanings so we can filter them
+  const [displayedWords, setDisplayedWords] = useState(words);
+  const [displayedDefinitions, setDisplayedDefinitions] = useState(
+    meanings.map((m) => extractDefinition(m[0]))
+  );
+
+  useEffect(() => {
+    setDisplayedWords(words);
+  }, [words]);
+
+  useEffect(() => {
+    setDisplayedDefinitions(meanings.map((m) => extractDefinition(m[0])));
+  }, [meanings]);
+
   // Current selections
   const [selectedWord, setSelectedWord] = useState<string>();
   const [selectedDefinition, setSelectedDefinition] = useState<string>();
-
   // array of objects with a definition, and the word that it belongs to
-  const randomPairs = useMemo(
-    () =>
+  const [randomPairs, setRandomPairs] = useState<WordDefinitionPair[]>([]);
+
+  useEffect(() => {
+    setRandomPairs(
       randomise<WordDefinitionPair>(
         meanings.map((m, i) => [words[i], extractDefinition(m[0])])
-      ),
-    [meanings, words]
-  );
+      )
+    );
+  }, [meanings, words]);
 
   const getWordStyle = (w: string) =>
     selectedWord === w ? "bg-green-500" : "bg-slate-500 ";
@@ -37,7 +52,15 @@ const FlashCardGrid = ({
   useEffect(() => {
     if (!selectedWord || !selectedDefinition) return;
 
-    console.log(isMatch(randomPairs, [selectedWord, selectedDefinition]));
+    if (isMatch(randomPairs, [selectedWord, selectedDefinition])) {
+      setDisplayedWords((words) => words.filter((w) => w !== selectedWord));
+      setDisplayedDefinitions((definitions) =>
+        definitions.filter((d) => d !== selectedDefinition)
+      );
+    } else {
+      setSelectedWord(undefined);
+      setSelectedDefinition(undefined);
+    }
   }, [selectedWord, selectedDefinition, randomPairs]);
 
   // we map over the randomized pair, and render the random word, with a definition that (probably) doesn't belong to it
@@ -45,9 +68,9 @@ const FlashCardGrid = ({
     <div className="p-8 flex flex-col">
       {title}
       <ul className="grid grid-cols-2 gap-8">
-        {randomPairs.map(([word], i) => {
-          const definition = extractDefinition(meanings[i]?.[0]);
-          const setDefinition = () => setSelectedDefinition(definition);
+        {displayedWords.map((word, i) => {
+          const setDefinition = () =>
+            setSelectedDefinition(displayedDefinitions[i]);
           const setWord = () => setSelectedWord(word);
 
           return (
@@ -58,10 +81,12 @@ const FlashCardGrid = ({
                 </button>
               </li>
               <li
-                className={`flex p-4 rounded ${getDefinitionStyle(definition)}`}
+                className={`flex p-4 rounded ${getDefinitionStyle(
+                  displayedDefinitions[i]
+                )}`}
               >
                 <button onClick={setDefinition} className="flex-1">
-                  {definition}
+                  {displayedDefinitions[i]}
                 </button>
               </li>
             </Fragment>
