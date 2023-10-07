a941b91f778a807374ea47cd05354ff6343c0f90
Ioannis Chrysostomakis <ic768@protonmail.com>
2023-10-08 01:57:19 +0300

    Minor changes to confetti hook

--- features/flashcards/hooks/useConfetti.ts
+++ features/flashcards/hooks/useConfetti.ts
@@ -1,30 +1,25 @@
 import { useCallback, useRef } from "react";
 
 export const useConfetti = () => {
-  const refConfetti = useRef<any | null>(null);
+  const refConfetti = useRef<any>(null);
 
-  const getInstance = useCallback((instance: any | null) => {
-    if (instance) {
-      refConfetti.current = instance;
-    }
+  const getInstance = useCallback((instance: any) => {
+    refConfetti.current = instance;
   }, []);
 
-  const makeShot = useCallback((particleRatio: any, opts: any) => {
-    refConfetti.current &&
-      refConfetti.current({
-        ...opts,
-        origin: { y: 0.7 },
-        particleCount: Math.floor(200 * particleRatio),
-      });
+  const makeShot = useCallback((opts: {}) => {
+    refConfetti?.current(opts);
   }, []);
 
   const onWin = useCallback(() => {
-    makeShot(2.85, {
+    makeShot({
       spread: 140,
       startVelocity: 55,
       gravity: 0.2,
       ticks: 60,
       scalar: 1,
+      origin: { y: 0.7 },
+      particleCount: 200,
     });
   }, [makeShot]);
 
