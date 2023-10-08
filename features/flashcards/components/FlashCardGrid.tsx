7d6ec6f2adbded20c429b3918ec43b62bf66ffe9
Ioannis Chrysostomakis <ic768@protonmail.com>
2023-10-08 15:58:10 +0300

    useFlashcardGame hook

--- features/flashcards/components/FlashCardGrid.tsx
+++ features/flashcards/components/FlashCardGrid.tsx
@@ -1,11 +1,9 @@
-import { Fragment, useEffect, useState } from "react";
+import { Fragment } from "react";
 
 import { WordMeanings } from "features/words";
-
-import { WordDefinitionPair } from "../types";
-import { isMatch, randomise, extractDefinition } from "../utils";
-import FlashCardGroup from "./FlashCardGroup";
+import useFlashCardGame from "../hooks/useFlashCardGame";
 import FlashCardItem from "./FlashCardItem";
+import { FlashCardGroup } from "context";
 
 interface FlashCardGridProps {
   group: FlashCardGroup;
@@ -18,57 +16,15 @@ const FlashCardGrid = ({
   meanings,
   onWin,
 }: FlashCardGridProps) => {
-  // Use these to display words and meanings so we can filter them
-  const [displayedWords, setDisplayedWords] = useState(words);
-  const [displayedDefinitions, setDisplayedDefinitions] = useState<string[]>(
-    []
-  );
+  const [
+    displayedWords,
+    displayedDefinitions,
+    getWordStyle,
+    getDefinitionStyle,
+    setSelectedWord,
+    setSelectedDefinition,
+  ] = useFlashCardGame(words, meanings, onWin);
 
-  // Current selections
-  const [selectedWord, setSelectedWord] = useState<string>();
-  const [selectedDefinition, setSelectedDefinition] = useState<string>();
-  // array of objects with a definition, and the word that it belongs to
-  const [randomPairs, setRandomPairs] = useState<WordDefinitionPair[]>([]);
-
-  useEffect(() => {
-    setDisplayedWords(words);
-  }, [words]);
-
-  useEffect(() => {
-    setDisplayedDefinitions(meanings.map((m) => extractDefinition(m[0])));
-  }, [meanings]);
-
-  useEffect(() => {
-    setRandomPairs(
-      randomise<WordDefinitionPair>(
-        meanings.map((m, i) => [words[i], extractDefinition(m[0])])
-      )
-    );
-  }, [meanings, words]);
-
-  // Check to see if a definition and a word have been selected to check for a match
-  useEffect(() => {
-    if (!selectedWord || !selectedDefinition) return;
-
-    if (isMatch(randomPairs, [selectedWord, selectedDefinition])) {
-      setDisplayedWords((words) => words.filter((w) => w !== selectedWord));
-      setDisplayedDefinitions((definitions) =>
-        definitions.filter((d) => d !== selectedDefinition)
-      );
-      onWin();
-    }
-
-    setSelectedWord(undefined);
-    setSelectedDefinition(undefined);
-  }, [selectedWord, selectedDefinition, randomPairs, onWin]);
-
-  const getWordStyle = (w: string) =>
-    selectedWord === w ? "bg-green-500" : "bg-slate-500 ";
-
-  const getDefinitionStyle = (d: string) =>
-    d && selectedDefinition === d ? "bg-orange-500" : "bg-slate-500 ";
-
-  // we map over the randomized pair, and render the random word, with a definition that (probably) doesn't belong to it
   return (
     <div className="p-8 flex flex-col">
       {title}
---
+++ features/flashcards/hooks/useFlashCardGame.ts
@@ -0,0 +1,74 @@
+import { useEffect, useState } from "react";
+
+import { WordMeanings } from "features/words";
+import { WordDefinitionPair } from "../types";
+import { extractDefinition, isMatch, randomise } from "../utils";
+
+const useFlashCardGame = (
+  words: string[],
+  meanings: WordMeanings[],
+  onWin: () => void
+) => {
+  // pairs of words with their corresponding definitions
+  const [randomPairs, setRandomPairs] = useState<WordDefinitionPair[]>([]);
+
+  // word / definition the user has clicked to match
+  const [selectedWord, setSelectedWord] = useState<string>();
+  const [selectedDefinition, setSelectedDefinition] = useState<string>();
+
+  // to omit already matched words and definitions
+  const [displayedWords, setDisplayedWords] = useState(words);
+  const [displayedDefinitions, setDisplayedDefinitions] = useState<string[]>(
+    []
+  );
+
+  useEffect(() => {
+    setDisplayedWords(words);
+  }, [words]);
+
+  useEffect(() => {
+    setDisplayedDefinitions(meanings.map((m) => extractDefinition(m[0])));
+  }, [meanings]);
+
+  // shuffle words and definitions
+  useEffect(() => {
+    setRandomPairs(
+      randomise<WordDefinitionPair>(
+        meanings.map((m, i) => [words[i], extractDefinition(m[0])])
+      )
+    );
+  }, [meanings, words]);
+
+  // logic when a word and definition have been selected
+  useEffect(() => {
+    if (!selectedWord || !selectedDefinition) return;
+
+    if (isMatch(randomPairs, [selectedWord, selectedDefinition])) {
+      setDisplayedWords((words) => words.filter((w) => w !== selectedWord));
+      setDisplayedDefinitions((definitions) =>
+        definitions.filter((d) => d !== selectedDefinition)
+      );
+      onWin();
+    }
+
+    setSelectedWord(undefined);
+    setSelectedDefinition(undefined);
+  }, [selectedWord, selectedDefinition, randomPairs, onWin]);
+
+  const getWordStyle = (w: string) =>
+    selectedWord === w ? "bg-green-500" : "bg-slate-500 ";
+
+  const getDefinitionStyle = (d: string) =>
+    d && selectedDefinition === d ? "bg-orange-500" : "bg-slate-500 ";
+
+  return [
+    displayedWords,
+    displayedDefinitions,
+    getWordStyle,
+    getDefinitionStyle,
+    setSelectedWord,
+    setSelectedDefinition,
+  ] as const;
+};
+
+export default useFlashCardGame;
