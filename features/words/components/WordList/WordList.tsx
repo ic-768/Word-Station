5ec9b7f6a18b52fd559ab989f27a117ae5d3b08b
Ioannis Chrysostomakis <ic768@protonmail.com>
2023-10-08 17:05:01 +0300

    Add index to key for word mapping

--- features/words/components/WordList/WordList.tsx
+++ features/words/components/WordList/WordList.tsx
@@ -6,8 +6,8 @@ import WordCard from "./WordCard";
 const WordList = ({ words }: { words: string[] }) => (
   <section className="w-full overflow-x-auto h-4/5">
     <div className="justify-center px-10 grid grid-cols-2 sm:grid-cols-4 md:grid-cols-4 lg:grid-cols-6 gap-5 lg:gap-7">
-      {words.map((w) => (
-        <WordCard key={w} word={w} />
+      {words.map((w, i) => (
+        <WordCard key={w + i} word={w} />
       ))}
     </div>
   </section>
