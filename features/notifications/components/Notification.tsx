8c6a912a8dc1a13bb93946d0fd3ca4a0faa2521b
Ioannis Chrysostomakis <ic768@protonmail.com>
2023-10-08 17:28:59 +0300

    Fix broken notification styles

--- features/notifications/components/Notification.tsx
+++ features/notifications/components/Notification.tsx
@@ -18,7 +18,7 @@ const Notification = ({
 
   return (
     <div
-      className={`top-4 z-20 fixed gap-4 inset-0 flex items-center px-8 py-2 justify-center px-8 py-2 mx-auto bg-white outline ${fgColor} ${bgColor} ${outlineColor} rounded-md w-4/6 sm:w-2/6 h-12 sm:h-16 animation-fill-forwards animate-fade-notification-in`}
+      className={`top-4 z-20 fixed gap-4 inset-0 flex items-center px-8 py-2 justify-center px-8 py-2 mx-auto outline ${fgColor} ${bgColor} ${outlineColor} rounded-md w-4/6 sm:w-2/6 h-12 sm:h-16 animation-fill-forwards animate-fade-notification-in`}
     >
       <FontAwesomeIcon icon={icon} className="h-5/6" />
       <span>{message}</span>
